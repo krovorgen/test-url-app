eff6aaf604eeb608cb4e0115e91ac0e664fa6a9d
maksim <krovorgen@gmail.com>
2023-05-25 11:04:21 +0300

    test

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,18 @@
-console.log('hello world');
+const link = document.querySelector("[data-open-link]");
+
+link!.addEventListener("click", function () {
+  let isSafariBrowser = false;
+  const url = link.dataset.openLink;
+
+  const ua = navigator.userAgent.toLowerCase();
+  if (ua.indexOf('safari') !== -1) {
+    isSafariBrowser = ua.indexOf('chrome') === -1;
+  }
+  if (isSafariBrowser) {
+    window.location.href = url;
+  } else {
+    window.open(url, '_blank');
+  }
+  return false;
+})
+
